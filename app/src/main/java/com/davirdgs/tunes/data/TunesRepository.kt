a63461fa39ce036af88379df629762e79a627ca9
davirds <davirds07@gmail.com>
2024-08-04 12:40:07 -0300

    add mappers

--- app/src/main/java/com/davirdgs/tunes/data/TunesRepository.kt
+++ app/src/main/java/com/davirdgs/tunes/data/TunesRepository.kt
@@ -1,29 +1,58 @@
 package com.davirdgs.tunes.data
 
+import com.davirdgs.tunes.data.model.Artist
+import com.davirdgs.tunes.data.model.Collection
+import com.davirdgs.tunes.data.model.Song
 import com.davirdgs.tunes.data.remote.ApiService
+import com.davirdgs.tunes.data.remote.model.BaseResponse
 import com.davirdgs.tunes.data.remote.model.Track
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 interface TunesRepository {
-    fun search(): Flow<List<Track>>
+    fun searchSongs(): Flow<List<Song>>
 }
 
 internal class TunesRepositoryImpl @Inject constructor(
     private val remoteSource: ApiService
 ) : TunesRepository {
 
-    override fun search() = flow {
+    override fun searchSongs() = flow {
         try {
             val response = remoteSource.search(
                 term = "Twenty one pilots",
                 limit = 25,
                 offset = 0
             )
-            emit(response.results)
+            emit(response.toSongList())
         } catch (e: Throwable) {
             e.printStackTrace()
         }
     }
 }
+
+private fun BaseResponse<Track>.toSongList(): List<Song> =
+    results.map(Track::toSongList)
+
+private fun Track.toSongList() =
+    Song(
+        artist = toArtist(),
+        collection = toCollection(),
+        artworkUrl = artworkUrl100,
+        id = trackId,
+        name = trackName,
+        timeMillis = trackTimeMillis
+    )
+
+private fun Track.toArtist() =
+    Artist(
+        id = artistId,
+        name = artistName
+    )
+
+private fun Track.toCollection() =
+    Collection(
+        id = collectionId,
+        name = collectionName
+    )
