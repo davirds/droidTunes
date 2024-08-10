9bbff59aaed6c40874d07fe1e45084b4bed73ef8
davirds <davirds07@gmail.com>
2024-08-10 20:23:43 -0300

    fix SongsList

--- app/src/main/java/com/davirdgs/tunes/ui/component/SongsList.kt
+++ app/src/main/java/com/davirdgs/tunes/ui/component/SongsList.kt
@@ -2,7 +2,10 @@ package com.davirdgs.tunes.ui.component
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
@@ -14,13 +17,19 @@ import com.davirdgs.tunes.ui.feature.home.SongItem
 @Composable
 fun SongsList(
     modifier: Modifier = Modifier,
+    navbarPadding: PaddingValues = WindowInsets.navigationBars.asPaddingValues(),
     songs: List<Song>,
     onSongClick: (Song) -> Unit
 ) {
     LazyColumn(
         modifier = modifier
             .fillMaxSize(),
-        contentPadding = PaddingValues(16.dp),
+        contentPadding = PaddingValues(
+            start = 16.dp,
+            end = 16.dp,
+            top = 16.dp,
+            bottom = 16.dp + navbarPadding.calculateBottomPadding()
+        ),
         verticalArrangement = Arrangement.spacedBy(4.dp)
     ) {
         items(songs) { song ->
